4c1aa3d563d1f5acb63e5417bc89a3ce4a7a36af
Oliver Gingerich <Ogingerich5354@conestogac.on.ca>
2024-04-11 11:21:46 -0400

    Created shell for deleteAccount()

--- Main.cpp
+++ Main.cpp
@@ -8,7 +8,10 @@
 #define CHANGE_PASSWORD	4
 #define CHANGE_PROFILE	5
 #define MIN_PASS_LENGTH	6
-#define EXIT			7
+#define SEE_ACCOUNTS	6
+#define SEE_HISTORY		7
+#define DELETE_HISTORY	8
+#define EXIT			9
 #define MAX_ARRAY_SIZE	101
 
 typedef struct Account {
@@ -45,14 +48,29 @@ typedef struct Queue {
 
 bool isPasswordValid(char* pass);
 char* removeNewLine(char* userInput);
+char* collectUserInput(void);
+void createAccount(void);
 
 int main(void) {
 
-	int exit = 0;
+	int exitLoop = 0;
 	int userNum = 0;
 	char userInput[MAX_ARRAY_SIZE] = "";
+	struct Stack* stack = (Stack*)malloc(sizeof(Stack));
+	if (stack == NULL) {
+		printf("No memory.");
+		exit(EXIT);
+	}
+	stack->top = NULL;
+	struct Queue* queue = (Queue*)malloc(sizeof(Queue));
+	if (queue == NULL) {
+		printf("No memory.");
+		exit(EXIT);
+	}
+	queue->front = NULL;
+	queue->back = NULL;
 
-	while (exit != EXIT) {
+	while (exitLoop != EXIT) {
 		printf(" ===YOUR ACCOUNT MANAGER=== \n");
 		printf("1.\t Create Account\n");
 		printf("2.\t Delete Account\n");
@@ -60,14 +78,21 @@ int main(void) {
 		printf("4.\t Change an Account's Password\n");
 		printf("5.\t Change an Account's Profile Picture\n");
 		printf("6.\t See Accounts\n");
-		printf("7.\t Exit Program\n");
+		printf("7.\t See History\n");
+		printf("8.\t Delete History\n");
+		printf("9.\t Exit Program\n");
 
-		fgets(userInput, MAX_ARRAY_SIZE, stdin);
-		strcpy(userInput, removeNewLine(userInput));
+		strcpy(userInput, collectUserInput());
 		userNum = atoi(userInput);
 		if (userNum < 1 || userNum > 7) {
 			printf("Invalid input\n");
 			continue;
+		}
+
+		switch (userNum) {
+		case CREATE_ACCOUNT:
+			createAccount(stack);
+			break;
 		}
 	}
 
@@ -110,4 +135,41 @@ char* removeNewLine(char* userInput)
 		userInput[possibleNewline] = '\0';
 	}
 	return userInput;
+}
+
+char* collectUserInput(void)
+{
+	char userInput[MAX_ARRAY_SIZE] = "";
+	fgets(userInput, MAX_ARRAY_SIZE, stdin);
+	strcpy(userInput, removeNewLine(userInput));
+	return userInput;
+}
+
+void createAccount(Stack* stack) 
+{
+	int exit = 0;
+	char userName[MAX_ARRAY_SIZE] = "";
+	char pass[MAX_ARRAY_SIZE] = "";
+	char profileURL[MAX_ARRAY_SIZE] = "";
+
+	while (exit == 0) {
+		printf("Enter a Username: ");
+		strcpy(userName, collectUserInput());
+		//if (searchByUserName(userName) !=NULL){
+		//	printf("Username in Use: Try again\n");
+		//	continue;
+		//}
+		printf("Password must be at least 6 characters in length and contain: Number, Symbol (!@#$%&*_+=-,.?\n");
+		printf("Enter a Password: ");
+		strcpy(pass, collectUserInput());
+		if (isPasswordValid(pass) == false) {
+			printf("Password does not meet requirements.\n");
+			continue;
+		}
+		printf("Enter a PFP URL: ");
+		strcpy(profileURL, collectUserInput());
+		exit = EXIT;
+	}
+	//addNode(userName, pass, profileURL);
+	//push(stack, CREATE_ACCOUNT,userName, pass, profileURL);
 }
