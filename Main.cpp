b72fa720daa02b94cdca4a10210f4a68ea4edbf1
Oliver Gingerich <Ogingerich5354@conestogac.on.ca>
2024-04-11 10:45:01 -0400

    Created removeNewLine - Began main shell

--- Main.cpp
+++ Main.cpp
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 #define CREATE_ACCOUNT	1
 #define DELETE_ACCOUNT	2
@@ -7,6 +8,8 @@
 #define CHANGE_PASSWORD	4
 #define CHANGE_PROFILE	5
 #define MIN_PASS_LENGTH	6
+#define EXIT			7
+#define MAX_ARRAY_SIZE	101
 
 typedef struct Account {
 	char* username;
@@ -41,13 +44,38 @@ typedef struct Queue {
 }Queue;
 
 bool isPasswordValid(char* pass);
+char* removeNewLine(char* userInput);
 
 int main(void) {
+
+	int exit = 0;
+	int userNum = 0;
+	char userInput[MAX_ARRAY_SIZE] = "";
+
+	while (exit != EXIT) {
+		printf(" ===YOUR ACCOUNT MANAGER=== \n");
+		printf("1.\t Create Account\n");
+		printf("2.\t Delete Account\n");
+		printf("3.\t Change an Account's Username\n");
+		printf("4.\t Change an Account's Password\n");
+		printf("5.\t Change an Account's Profile Picture\n");
+		printf("6.\t See Accounts\n");
+		printf("7.\t Exit Program\n");
+
+		fgets(userInput, MAX_ARRAY_SIZE, stdin);
+		strcpy(userInput, removeNewLine(userInput));
+		userNum = atoi(userInput);
+		if (userNum < 1 || userNum > 7) {
+			printf("Invalid input\n");
+			continue;
+		}
+	}
 
 	return 0;
 }
 
-bool isPasswordValid(char* pass) {
+bool isPasswordValid(char* pass) 
+{
 	char nums[11] = "0123456789";
 	char symbols[15] = "!@#$%&*_+=-,.?";
 	char upper[27] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
@@ -71,4 +99,15 @@ bool isPasswordValid(char* pass) {
 	else {
 		return true;
 	}
+}
+
+char* removeNewLine(char* userInput)
+{
+	size_t stringLength = strlen(userInput);
+	unsigned int possibleNewline = stringLength - 1;
+	if (userInput[possibleNewline] == '\n')
+	{
+		userInput[possibleNewline] = '\0';
+	}
+	return userInput;
 }
